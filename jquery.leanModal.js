bef7879c258406d5eb47ac2d47520694e181ed57
Layon <layon1993@gmail.com>
2016-06-30 10:06:45 +0100

    Add changes to allow calling openModal. 	> add license

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,73 +1,70 @@
-(function($){
- 
-    $.fn.extend({ 
-         
-        leanModal: function(options) {
- 
-            var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null
-            }
-            
-            var overlay = $("<div id='lean_overlay'></div>");
-            
-            $("body").append(overlay);
-                 
-            options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-               
-                $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
+(function($) {
 
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
+  $.fn.leanModal = function(options) {
 
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+    var defaults = {
+      top: 100,
+      overlay: 0.5,
+      closeButton: null
+    }
 
-        		$('#lean_overlay').fadeTo(200,o.overlay);
+    var overlay = $("<div id='lean_overlay'></div>");
 
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
+    $("body").append(overlay);
 
-        		$(modal_id).fadeTo(200,1);
+    var options = $.extend(defaults, options);
 
-                e.preventDefault();
-                		
-              	});
-             
-            });
+    function close_modal(modal_id) {
 
-			function close_modal(modal_id){
+      $("#lean_overlay").fadeOut(200);
 
-        		$("#lean_overlay").fadeOut(200);
+      $(modal_id).css({
+        'display': 'none'
+      });
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
-        }
+    }
+    return this.each(function() {
+      var leanModalInstance = this
+      this.openModal = function(modal_id) {
+        var o = options;
+        var modal_id = modal_id || $(this).attr("href");
+
+        $("#lean_overlay").click(function() {
+          close_modal(modal_id);
+        });
+
+        $(o.closeButton).click(function() {
+          close_modal(modal_id);
+        });
+
+        var modal_height = $(modal_id).outerHeight();
+        var modal_width = $(modal_id).outerWidth();
+
+        $('#lean_overlay').css({
+          'display': 'block',
+          opacity: 0
+        });
+
+        $('#lean_overlay').fadeTo(200, o.overlay);
+
+        $(modal_id).css({
+          'display': 'block',
+          'position': 'fixed',
+          'opacity': 0,
+          'z-index': 11000,
+          'left': 50 + '%',
+          'margin-left': -(modal_width / 2) + "px",
+          'top': o.top + "px"
+        });
+
+        $(modal_id).fadeTo(200, 1);
+      }
+      $(this).data("leanModal", leanModalInstance)
+      $(this).click(function(e) {
+        $(this).data("leanModal").openModal();
+        e.preventDefault();
+      });
     });
-     
+  }
+
 })(jQuery);
